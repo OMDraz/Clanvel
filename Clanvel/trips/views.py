32ef1f6a3f259f4620a60b6c932953dfe06cae2a
Omar <omardraz94@gmail.com>
2021-02-01 21:48:47 -0800

    Added a form on my website

--- Clanvel/trips/views.py
+++ Clanvel/trips/views.py
@@ -1,10 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect 
+from django import forms
+from .forms import RegisterForm
 
 def index(request):
     context = {}
     return render(request, 'base.html', context)
 
 def signup(request):
-    context = {} 
+    context = {}
+    context['form'] = RegisterForm()
     return render(request, 'signup.html', context)
