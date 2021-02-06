46ba9348b4bbbc8db83d94bf7fa21f1782662dba
Omar <omardraz94@gmail.com>
2021-02-05 20:27:18 -0800

    added login and signup forms

--- Clanvel/trips/views.py
+++ Clanvel/trips/views.py
@@ -1,13 +1,21 @@
-from django.http import HttpResponse
-from django.shortcuts import render, redirect 
-from django import forms
-from .forms import RegisterForm
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
 
 def index(request):
     context = {}
     return render(request, 'base.html', context)
 
 def signup(request):
-    context = {}
-    context['form'] = RegisterForm()
-    return render(request, 'signup.html', context)
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
