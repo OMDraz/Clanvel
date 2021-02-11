f3feee4682a7ce3e94c15325bcafba24f7220b5d
Omar <omardraz94@gmail.com>
2021-02-10 22:53:10 -0800

    Small changes

--- Clanvel/trips/admin.py
+++ Clanvel/trips/admin.py
@@ -1,2 +1,7 @@
 from django.contrib import admin
+from .models import Profile 
 
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ("user", "email_address", "last_name", "location", "interested_in_friends")
+
+admin.site.register(Profile, ProfileAdmin)
